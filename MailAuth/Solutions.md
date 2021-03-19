e66f63c8bd449e44446114e75fe2bfec35468223
elaelheni <elheni.ela@gmail.com>
2021-03-19 10:51:22 +0100

    ajout des solutions

---
+++ MailAuth/Solutions.md
@@ -0,0 +1,12 @@
+# Solutions 
+
+## Pour installer toutes les librairies nécessaires :
+
+```
+pip install -r requirements.txt
+```
+
+## Compte gmail :
+
+Après la création du compte gmail, vous devez autoriser l'accès aux applications les moins sécurisées sur ce lien :
+https://myaccount.google.com/lesssecureapps
--- MailAuth/app.py
+++ MailAuth/app.py
@@ -17,9 +17,11 @@ from flask_bcrypt import Bcrypt
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from flask_mail import Mail, Message
 from flask_login import LoginManager, login_user, logout_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from itsdangerous import URLSafeTimedSerializer 
 
 
 
@@ -29,9 +31,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
 
 #Generé avec : import secrets, secrets.token_hex(16)
 app.config['SECRET_KEY'] = '30619d162891c7cbf8342dc2633a57b5'
+app.config['SECURITY_PASSWORD_SALT'] = '667f6454014313669bf5d9c4d7eb4e04'
 
+app.config['MAIL_SERVER'] = "smtp.googlemail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "exemples.inf5190@gmail.com"
+app.config['MAIL_PASSWORD'] = "Secret..123"
+
+
+mail = Mail(app)
 
 db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
@@ -51,7 +63,7 @@ class User(db.Model):
     date_publication = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     mdp = db.Column(db.String(60), nullable=False)
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
-"""
+
 
 class Inscription(FlaskForm):
     nom = StringField("Nom de l'utilisateur", validators=[DataRequired()])
@@ -75,6 +87,33 @@ class Login(FlaskForm):
     submit = SubmitField('Se connecter')
 
 
+def envoyer_courriel(jeton, email):
+    msg = Message("Courriel d'activation", sender=app.config['MAIL_USERNAME'], recipients=[email])
+    msg.body = f'''
+        Pour activer votre compte cliquez ici :
+        {url_for('activer_mail', jeton=jeton, _external=True)}
+
+        Si vous n'avez pas fait cette demande ignorez ce courriel!
+    '''
+    mail.send(msg)
+
+def generer_jeton_activation(email):
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return s.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+def confirmer_jeton(jeton, expiration=3600):
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = s.loads(
+            jeton,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
@@ -89,16 +128,20 @@ def home():
         user = User(nom=form.nom.data, prenom=form.prenom.data, email=form.email.data, mdp=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Votre compte a été créé avec succès, Vous pouvez vous connectez!', 'success')
+
+        jeton = generer_jeton_activation(user.email)
+        envoyer_courriel(jeton, user.email)
+
+        flash('Un courriel de validation vous a été envoyé!', 'success')
         return redirect(url_for('login'))
 
     else:
 
         return render_template('inscription.html', form=form)
 
-@app.route('/confirmation')
-def confirmation():
-    return render_template('confirmation.html')
+@app.route('/confirmation/<nom>/<prenom>')
+def confirmation(nom, prenom):
+    return render_template('confirmation.html', nom=nom, prenom=prenom)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -106,7 +149,8 @@ def login():
     if request.method=="POST" and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.mdp, form.password.data):
-            return redirect(f'/confirmation')
+            return redirect(f'/confirmation/{user.nom}/{user.prenom}')
+            
         else:
             flash('Connexion impossible, merci de vérifier votre adresse mail et votre mot de passe.', 'danger')
 
@@ -118,4 +162,21 @@ def logout():
     logout_user()
     flash('Vous etes déconnecté !', 'success')
     return redirect(url_for('home'))
-    """
+
+
+@app.route('/activation/<jeton>')
+def activer_mail(jeton):
+    try:
+        email = confirmer_jeton(jeton)
+    except:
+        flash('Le lien est invalide ou a expiré', 'warning')
+    user = User.query.filter_by(email=email).first_or_404()
+    if user.confirmed :
+        flash('Le compte a déjà été confirmé, merci de vous connecter', 'success')
+        return redirect(url_for('login'))
+    else:
+        user.confirmed = True
+        db.session.add(user)
+        db.session.commit()
+        flash ('Vous avez confirmé votre compte, vous pouvez vous connecter', 'success')
+    return redirect(f'/login')
